8b648a83ca5eb6b651726833424195824cb96b3e
Philip Otter <78989650+Philip-Otter@users.noreply.github.com>
2026-03-08 21:41:02 -0800

    Add Nix shell configuration for assembly tools
    Create a Nix shell environment for assembly development with necessary tools.

---
+++ asm-x86/shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {                
+
+  buildInputs = with pkgs; [   
+    binutils   
+    nasm
+    gdb       
+  ];
+
+  shellHook = ''
+    RED='\e[1;31m'
+    BLUE='\e[1;34m'
+    GREEN='\e[1;32m'
+    CYAN='\e[1;36m'
+    RESET='\e[0m'
+
+    export PS1="\n\[$CYAN\]SHELLED{\[$RED\][ASSEMBLY]\[$GREEN\]\u\[$RESET\]:\[$BLUE\]\w\[$CYAN\]}\[$RESET\] \$ "
+
+    echo -e " \n $CYAN>> Assembly Environment Ready$RESET"
+    echo -e " Tools loaded: $GREEN binutils, nasm, gdb $RESET \n"
+  '';
+}
