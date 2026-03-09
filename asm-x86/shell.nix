46f962d5065525a7f356b06378699cf384df48c7
Philip Otter <78989650+Philip-Otter@users.noreply.github.com>
2026-03-09 00:04:37 -0800

    Add debugging tools to assembly shell environment

--- asm-x86/shell.nix
+++ asm-x86/shell.nix
@@ -4,7 +4,10 @@ pkgs.mkShell {
   buildInputs = with pkgs; [   
     binutils   
     nasm
-    gdb       
+    gdb
+    gef
+    cutter
+    ghidra       
   ];
 
   shellHook = ''
@@ -17,6 +20,6 @@ pkgs.mkShell {
     export PS1="\n\[$CYAN\]SHELLED{\[$RED\][ASSEMBLY]\[$GREEN\]\u\[$RESET\]:\[$BLUE\]\w\[$CYAN\]}\[$RESET\]\$ "
 
     echo -e " \n $CYAN>> Assembly Environment Ready$RESET"
-    echo -e " Tools loaded: $GREEN binutils, nasm, gdb $RESET \n"
+    echo -e " Tools loaded: $GREEN binutils, nasm, gdb, gef, cutter, ghidra $RESET \n"
   '';
 }
